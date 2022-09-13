23f9393cb9b07ccba69aaa7915afb4af6328fbc2
kartikmax <kyadav5213@gmail.com>
2022-09-13 17:18:06 +0530

    adding mui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+const myStyle = {
+  backgroundColor: "lightBlue",
+};
 
 const App = () => {
-  const myStyle={
-    backgroundColor:"lightBlue"
-  }
-  return (
-    <div style={myStyle} >
-      hello world this is version control
-    </div>
-  )
-}
+  const [countryData, setCountryData] = useState();
+ 
 
-export default App
+
+  function getData() {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((json) => {
+        setCountryData(json);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(countryData);
+  return (
+    <div style={myStyle}>
+      hello world this is version control
+      <h1>this is the table</h1>
+      
+    </div>
+  );
+};
+
+export default App;
