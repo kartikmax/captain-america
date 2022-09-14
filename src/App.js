56d83371a36039332995ee0c44ff498e01f3e6b6
kartikmax <kyadav5213@gmail.com>
2022-09-14 10:56:27 +0530

    responsivenses added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import Main from './Components/Main'
+import React, { useState } from "react";
+import Main from "./Components/Main";
+import "./App.css";
 
 export default function App() {
+  const [enable, setEnable] = useState(true);
+  function callFn() {
+    enable ? setEnable(false) : setEnable(true);
+  }
+  console.log(enable, "check");
+
   return (
     <>
-      <Main/>
+      <h1>Countries App</h1>
+      {enable ? (
+        <>
+          <div className="container">
+            <div className="child child-1" onClick={callFn}>
+              <div className="innerText">All Details</div>
+            </div>
+            <div className="child child-2" onClick={callFn}>
+              <div className="innerText">Analytics</div>
+            </div>
+            <div className="child child-3" onClick={callFn}>
+              <div className="innerText">Add Country</div>
+            </div>
+            <div className="child child-4" onClick={callFn}>
+              <div className="innerText">Chart</div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Main />
+      )}
     </>
-  )
+  );
 }
